de62b136e03cbe0d59d9a4cad090660691078991
Alie Stone <helloaliestone@gmail.com>
2020-09-14 17:27:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,13 @@
 def my_each(array)
+  array = ["Alie, "Liz", "Sascha"]
+    i = 0
+    while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+end
+
+def my_each(array)
 ["Tim", "Tom", "Jim"].each do |name|
     if name.start_with?("T")
       puts "Hi, #{name}"

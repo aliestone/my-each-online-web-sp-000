42256ab076577296ad63a2a6a23643cc127443ed
Alie Stone <helloaliestone@gmail.com>
2020-09-14 17:41:02 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,9 @@
-def my_each (array)
-  array = ["Alie, Liz, Sascha"]
-  array.each do |item |
-    puts "names"
-        i = 0
-      while i < array.length
-    yield array[i]
-    i = i + 1
-    end
-    array
+def my_each(array) 
+  i = 0
+  while i < array.size
+    block_given? 
+    yield(array[i]) 
+    i += 1
   end
+array
 end
